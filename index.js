60f26a8817a05d50321e8016d97e6d3917b39607
goskan93 <natalia.goska@gmail.com>
2023-03-27 16:22:34 +0200

    cli

--- index.js
+++ index.js
@@ -1,40 +1,38 @@
-// import yargs from 'yargs'
-
-// yargs()
-//     .options({
-//       'e': {
-//         demandOption: true,
-//         alias: ["email"],
-//         string: true,
-//         desc: "E-mail to login to chatGPT"
-//       },
-//       'p' : {
-//         demandOption: true,
-//         alias: ["password"],
-//         string: true,
-//         desc: "Password to login to chatGPT"
-//       },
-//       'q' : {
-//         demandOption: true,
-//         alias: ["question"],
-//         string: true,
-//         desc: "Question for chatGPT"
-//       }
-//     })
-//     .help()
-//     .alias('help', 'h')
-//     .argv
-
+ import yargs from 'yargs'
+ import { hideBin } from 'yargs/helpers';
 import { CrawlerGPT } from './crawler.js';
 import { sleep } from './sleep.js';
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config({path: `.env.local`})
 
-const email = process.env.EMAIL;
-const pass = process.env.PASS;
+const argv = yargs(hideBin(process.argv))
+    .usage('Hello! Try: node $0 -e [string] -p [string] -q "[string]"')
+    .options({
+      'e': {
+        demandOption: true,
+        alias: ["email"],
+        string: true,
+        desc: "E-mail to login to chatGPT"
+      },
+      'p' : {
+        demandOption: true,
+        alias: ["password"],
+        string: true,
+        desc: "Password to login to chatGPT"
+      },
+      'q' : {
+        demandOption: true,
+        alias: ["question"],
+        string: true,
+        desc: "Question for chatGPT"
+      }
+    })
+    .demandOption(["e", "p", "q"])
+    .argv
 
-const crawler = new CrawlerGPT(email, pass);
-await crawler.askGPT("What is the capital of Sweden");
+const { email, password, question } = argv;
+console.log("Wait for an answer...")
+
+const crawler = new CrawlerGPT(email, password);
+await crawler.askGPT(question);
 await sleep(10000);
 const responseArray = await crawler.getResponse();
 const responseClean =  [...new Set(responseArray)]
@@ -42,4 +40,4 @@ const responseClean =  [...new Set(responseArray)]
     .map(text => text.slice(-2) === "/n" ? text.slice(0,-2) : text)
 
 
-responseClean.map( t => console.log(t))
+ responseClean.map( t => console.log(t))
